72c376040902e6382dd0b0ed72b83f08db3b90c6
Palharez <xeduardopalhares@gmail.com>
2019-04-24 20:23:07 -0300

    Update testes do funcionario

--- tests/test_funcionario.py
+++ tests/test_funcionario.py
@@ -1,6 +1,7 @@
 import os, sys
 
 from utils import truncate, insert
+from utils_auth import AuthActions
 
 root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(root)
@@ -15,8 +16,16 @@ class TestFuncionario(unittest.TestCase):
     def setUp(self):            
         app = create_app('tcc_tests')
         self.app_test = app.test_client()
+        self.auth = AuthActions(self.app_test)
 
     def test_get_login_page(self):
         response = self.app_test.get('/')
-
         self.assertEqual(200, response.status_code)
+
+    def test_login(self):
+        assert self.app_test.get('/').status_code == 200
+        response = self.auth.login()
+
+    def test_logout(self):
+       self.auth.login()
+       self.auth.logout()
